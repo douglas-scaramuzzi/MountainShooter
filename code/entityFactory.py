dbd41f890ed7d718e1c0744e30b364d50045ee4c
Douglas <douglas.scaramuzzi@gmail.com>
2025-05-17 18:53:34 -0300

    Level 2 implementation

--- code/entityFactory.py
+++ code/entityFactory.py
@@ -10,14 +10,19 @@ from code.player import Player
 
 class EntityFactory:
     @staticmethod
-
     def get_entity(entity_name: str, position=(0, 0)):
         match entity_name:
             case 'Level1Bg':
                 list_bg = []
-                for i in range(7):
+                for i in range(7):  # level1bg images number
                     list_bg.append(Background(f'Level1Bg{i}', position=(0, 0)))
                     list_bg.append(Background(f'Level1Bg{i}', position=(WIN_WIDTH, 0)))
+                return list_bg
+            case 'Level2Bg':
+                list_bg = []
+                for i in range(5):  # level2bg images number
+                    list_bg.append(Background(f'Level2Bg{i}', position=(0, 0)))
+                    list_bg.append(Background(f'Level2Bg{i}', position=(WIN_WIDTH, 0)))
                 return list_bg
             case 'Player1':
                 return Player(name='Player1', position=(10, WIN_HEIGHT / 2 - 30))
@@ -29,5 +34,3 @@ class EntityFactory:
                 return Enemy(name='Enemy2', position=(WIN_WIDTH + 10, random.randint(13, WIN_HEIGHT - 13)))
             case _:
                 return None
-
-
--- code/level.py
+++ code/level.py
@@ -13,7 +13,8 @@ from pygame import Surface, Rect
 from pygame.font import Font
 
 from code import EntityMediator
-from code.Const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME, C_GREEN, C_CYAN
+from code.Const import C_WHITE, WIN_HEIGHT, MENU_OPTION, EVENT_ENEMY, SPAWN_TIME, C_GREEN, C_CYAN, EVENT_TIMEOUT, \
+    TIMEOUT_STEP, TIMEOUT_LEVEL
 from code.enemy import Enemy
 from code.entity import Entity
 from code.entityFactory import EntityFactory
@@ -26,17 +27,23 @@ def should_remove(ent):
 
 
 class Level:
-    def __init__(self, window, name, game_mode):
-        self.timeout = 20000  # 20 segundos
+    def __init__(self, window: Surface, name: str, game_mode: str, player_score: list[int]):
+        self.timeout = TIMEOUT_LEVEL
         self.window = window
         self.name = name
         self.game_mode = game_mode
         self.entity_list: list[Entity] = []
-        self.entity_list.extend(EntityFactory.get_entity('Level1Bg'))
-        self.entity_list.append(EntityFactory.get_entity('Player1'))
+        self.entity_list.extend(EntityFactory.get_entity(self.name + 'Bg'))
+        player = EntityFactory.get_entity('Player1')
+        player.score = player_score[0]
+        self.entity_list.append(player)
         if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
-            self.entity_list.append(EntityFactory.get_entity('Player2'))
+            player = EntityFactory.get_entity('Player2')
+            player.score = player_score[1]
+            self.entity_list.append(player)
         pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
+        pygame.time.set_timer(EVENT_TIMEOUT, TIMEOUT_STEP) #100ms
+
 
     def _should_remove(self, ent):
         # Remove tiros que saíram da tela
@@ -56,7 +63,7 @@ class Level:
         return False
 
 
-    def run(self):
+    def run(self, player_score: list[int]):
         pygame.mixer_music.load(f'./asset/{self.name}.mp3')
         pygame.mixer_music.play(-1)
         clock = pygame.time.Clock()
@@ -75,6 +82,25 @@ class Level:
                 if event.type == EVENT_ENEMY:
                     choice = random.choice(('Enemy1', 'Enemy2'))
                     self.entity_list.append(EntityFactory.get_entity(choice))
+                if event.type == EVENT_TIMEOUT:
+                    self.timeout -= TIMEOUT_STEP
+                    if self.timeout == 0:
+                        for ent in self.entity_list:
+                            if isinstance(ent, Player) and ent.name == 'Player1':
+                                player_score[0] = ent.score
+                            if isinstance(ent, Player) and ent.name == 'Player2':
+                                player_score[1] = ent.score
+                        return True
+
+
+            found_player = False
+            for ent in self.entity_list:
+                if isinstance(ent, Player):
+                    found_player = True
+                    break
+
+            if not found_player:
+                return False
 
             # Atualiza e desenha entidades
             for ent in self.entity_list:
